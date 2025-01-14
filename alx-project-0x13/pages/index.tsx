49945b08b4fef419a2101933c55e52ac02d7eb90
Ozokwelu Victor <ozokweluvic@gmail.com>
2025-01-14 11:05:53 -0800

    added custom hook

--- alx-project-0x13/pages/index.tsx
+++ alx-project-0x13/pages/index.tsx
@@ -1,45 +1,30 @@
 import ImageCard from "@/components/common/ImageCard";
+import useFetchData from "@/hooks/useFetchData";
 import { ImageProps } from "@/interfaces";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
-  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { isLoading, responseData, generatedImages, fetchData } = useFetchData<
+    any,
+    { prompt: string }
+  >();
 
-  const handleGenerateImage = async () => {
-    setIsLoading(true);
-    const resp = await fetch("/api/generate-image", {
-      method: "POST",
-      body: JSON.stringify({
-        prompt,
-      }),
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-
-    if (!resp.ok) {
-      setIsLoading(false);
-      return;
-    }
-
-    const data = await resp.json();
-    setIsLoading(false);
-    setImageUrl(data?.message);
-    setGeneratedImages((prev) => [
-      ...prev,
-      { imageUrl: data?.message, prompt },
-    ]);
+  const handleGenerateImage = () => {
+    fetchData("/api/generate-image", { prompt });
   };
+
+  useEffect(() => {
+    if (!isLoading) {
+      setImageUrl(responseData?.message);
+    }
+  }, [isLoading]);
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mb-2 text-black">
-          Image Generation App
-        </h1>
+        <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
         <p className="text-lg text-gray-700 mb-4">
           Generate stunning images based on your prompts!
         </p>
@@ -50,7 +35,7 @@ const Home: React.FC = () => {
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
-            className="w-full p-3 border border-gray-300 rounded-lg mb-4 text-black"
+            className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
           <button
             onClick={handleGenerateImage}
@@ -59,7 +44,7 @@ const Home: React.FC = () => {
           </button>
         </div>
 
-        {imageUrl && (
+        {responseData?.message && (
           <ImageCard
             action={() => setImageUrl(imageUrl)}
             imageUrl={imageUrl}
